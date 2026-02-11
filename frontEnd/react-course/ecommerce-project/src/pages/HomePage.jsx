cc7eaaac2e857ac2d5919368738ab7e90fd3bf7d
ricardo <ricardo.alvear.personal@gmail.com>
2026-02-11 15:15:18 -0500

    Add server provy config for /api and /images

--- frontEnd/react-course/ecommerce-project/src/pages/HomePage.jsx
+++ frontEnd/react-course/ecommerce-project/src/pages/HomePage.jsx
@@ -8,10 +8,10 @@ export function HomePage() {
   const [cart, setCart] = useState([])
 
   useEffect(() => {
-    axios.get("http://localhost:3000/api/products").then((res) => {
+    axios.get("/api/products").then((res) => {
       setProducts(res.data);
     });
-    axios.get("http://localhost:3000/api/cart-items").then((res) => {
+    axios.get("/api/cart-items").then((res) => {
       setCart(res.data)
     })
   }, []);
